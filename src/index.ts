a47623854cda6aa8aeb276f7ade5141bca111b37
Gami19 <k.ikegami.15@gmail.com>
2026-07-29 19:39:29 +0900

    feat: /notesを追加

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
 const port = 3000;
 
+app.use(express.json())
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, Notes API!');
+})
+
+app.post('/notes', (req: Request, res: Response) => {
+    console.log(req.body)
+    res.send('メモを受け取りました');
 })
 
 app.listen(port, () =>{
