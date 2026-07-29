787ee5c8e672eb22054f30d66266e927c1a8ecbe
Gami19 <k.ikegami.15@gmail.com>
2026-07-29 20:37:09 +0900

    feat: データを保存するシステムを追加

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express, { type Express, type Request, type Response } from 'express';
+import path from 'node:path';
+import fs from 'node:fs';
 
 const app: Express = express();
 const port = 3000;
+
+// 保存先パスを作成
+const uploadDir = path.join(process.cwd(), 'public');
 
 app.use(express.json())
 
@@ -9,9 +14,23 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello, Notes API!');
 })
 
-app.post('/notes', (req: Request, res: Response) => {
-    console.log(req.body)
-    res.send('メモを受け取りました');
+app.post('/notes', async(req: Request, res: Response) => {
+
+    // data に格納
+    const data = req.body.note;
+
+    // ファイルタイトル
+    const fileName = `${req.body.title}.md`;
+    const filePath = path.join(uploadDir, fileName);
+
+    // /public に保存
+    await fs.writeFile(filePath, data, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.send('メモを保存しました');
+        }  
+    })
 })
 
 app.listen(port, () =>{
