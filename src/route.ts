ca251456c63d010abc31cbf2608f8444b4a7dfde
Gami19 <k.ikegami.15@gmail.com>
2026-07-30 19:31:22 +0900

    feat: retrieve a specific note by filename

--- src/route.ts
+++ src/route.ts
@@ -45,4 +45,29 @@ router.get('/notes',async(req: Request, res: Response) => {
     }
 })
 
+// get a specific MD 
+router.get('/notes/:filename', async(req: Request, res: Response) => {
+
+    // get parameters
+    const targetFile= req.params.filename
+
+    // check params
+    if (typeof targetFile === "string"){
+
+        const markdownPath = path.join(uploadDir, `${targetFile}.md`);
+
+        // read the MD
+        try{
+            const fileContent = await fs.readFile(markdownPath, 'utf-8');
+            res.send(fileContent);
+            console.log('特定のファイルを送信')
+        }catch(error){
+            console.log(error);
+        }
+
+    } else {
+        res.status(400).send("Not params string");
+    }
+
+})
 export default router
