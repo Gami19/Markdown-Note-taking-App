7c6c00798a56a092bb9aebaee58dabb08c29b603
Gami19 <k.ikegami.15@gmail.com>
2026-07-30 14:00:57 +0900

    feat: routeへの切り出し

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,11 @@
-import express, { type Express, type Request, type Response } from 'express';
-import path from 'node:path';
-import fs from 'node:fs/promises';
+import express, { type Express } from 'express';
+import router from './route.js'
 
 const app: Express = express();
 const port = 3000;
 
-// 保存先パスを作成
-const uploadDir = path.join(process.cwd(), 'public');
-
 app.use(express.json())
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Notes API!');
-})
-
-app.post('/notes', async(req: Request, res: Response) => {
-
-    // リクエストを保持
-    const fileData = req.body.note;
-
-    // ファイルタイトル
-    const fileName = `${req.body.title}.md`;
-    const filePath = path.join(uploadDir, fileName);
-
-    // /public に保存
-    try{
-        await fs.writeFile(filePath, fileData);
-        res.send('メモを保存しました');
-    } catch(error){
-        console.log(error);
-    }
-})
+app.use(router)
 
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`);
